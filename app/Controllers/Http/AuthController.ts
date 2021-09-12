62857a2121bbc49f86326b7cbe4b280693ecfc6f
Noman Abdullah <71120198+nomanabdullah-dev@users.noreply.github.com>
2021-09-13 00:28:52 +0600

    api for login done

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,9 +1,10 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import User from 'App/Models/User'
+import authConfig from 'Config/auth'
 
-export default class RegistersController {
-  async index({request, response}:HttpContextContract)
+export default class AuthController {
+  async register({request, response}:HttpContextContract)
   {
     const validations = schema.create({
       email: schema.string({}, [
@@ -20,5 +21,15 @@ export default class RegistersController {
     const user = await User.create(data)
 
     return response.created(user)
+  }
+
+  async login({request, auth}: HttpContextContract)
+  {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    const token = await auth.attempt(email, password)
+
+    return token.toJSON()
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,6 @@ Route.group(()=>{
     Route.post('/todo', 'TodosController.store')
     Route.patch('/todo/:id', 'TodosController.update')
   }).middleware('auth')
-  Route.post('/register', 'RegistersController.index')
+  Route.post('/register', 'AuthController.register')
+  Route.post('/login', 'AuthController.login')
 }).prefix('api')
